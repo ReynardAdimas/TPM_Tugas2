5c8b350694b9bd109d3e6732d95be56c5065f338
ReynardAdimas <dimastube22@gmail.com>
2026-03-11 21:37:18 +0700

    Cek fitur login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isPasswordHidden = true;
+  final TextEditingController _usernameController = TextEditingController(); 
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    void login() {
+      String usn = _usernameController.text; 
+      String pwd = _passwordController.text; 
+
+      if(usn == "subagus" && pwd == "kriptogaasik") {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Login Berhasil"),
+            backgroundColor: Colors.green,
+            )
+        );
+      } else {
+        if(usn == "" && pwd == "") {
+          ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Username dan Password Harap Diisi"),
+            backgroundColor: Colors.red,
+            )
+        );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Username atau Password Salah"),
+            backgroundColor: Colors.red,
+            )
+        ); 
+        _usernameController.clear(); 
+        _passwordController.clear();
+        }
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                label: Text("Masukkan Username")
+              ),
+            ), 
+            SizedBox(height: 10,), 
+            TextField(
+              controller: _passwordController,
+              obscureText: isPasswordHidden,
+              decoration: InputDecoration(
+                label: Text("Masukkan Password"), 
+                suffixIcon: IconButton(
+                  onPressed: (){
+                    setState(() {
+                      isPasswordHidden = !isPasswordHidden;
+                    });
+                  }, 
+                  icon: Icon(
+                    isPasswordHidden ? Icons.visibility_off : Icons.visibility
+                  )
+                )
+              ),
+            ), 
+            SizedBox(height: 10,), 
+            ElevatedButton(
+              onPressed: (){
+                login();
+              }, 
+              child: Text("Login")
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
